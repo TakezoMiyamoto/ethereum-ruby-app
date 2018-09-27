8c4fd1977136c69ac9d429fd5fdb10b250783d1e
takezo <takezo.m.1987@gmail.com>
2018-09-27 12:38:24 +0900

    modify contract code

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,8 +3,8 @@ class TopController < ApplicationController
   GANACHE_URL = 'HTTP://127.0.0.1:7545'
 
   def index
-    @client = Ethereum::HttpClient.new(GANACHE_URL) #①
-    @contract = Ethereum::Contract.create(file: ETHEREUM_TOKEN_PATH, client: @client) #②
-    @contract.deploy_and_wait('Hello, World!') #③
+    @client = Ethereum::HttpClient.new(GANACHE_URL) # ①
+    @contract = Ethereum::Contract.create(file: ETHEREUM_TOKEN_PATH, client: @client) # ②
+    @contract.deploy_and_wait('Hello, World!') # ③
   end
 end
--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,23 +1,23 @@
 pragma solidity ^0.4.24;
 
 contract HelloWorld {
-  string public greeting;
-  address public owner;
+    string public greeting;
+    address public owner;
 
-  function HelloWorld(string _greeting) {
-    greeting = _greeting;
-    owner = msg.sender;
-  }
+    function HelloWorld(string _greeting) {
+        greeting = _greeting;
+        owner = msg.sender;
+    }
 
-  function say() constant returns (string) {
-    return greeting;
-  }
+    function say() constant public returns (string) {
+        return greeting;
+    }
 
-  function getBalance() constant returns (uint balance) {
-    return owner.balance;
+    function getBalance() constant public returns (uint balance) {
+        return owner.balance;
+    }
 
-  function getAddress() constant returns (address) {
-    return owner;
-  }
-
+    function getAddress() constant public returns (address) {
+        return owner;
+    }
 }
